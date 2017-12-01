22b4222abe222f79c32466e217e63afa4f7b4f97
milk9111 <connorlndbrg349@gmail.com>
2017-12-01 07:39:51 -0800

    Can now open the updatePatient screen on button click for doctor and it will check the fields before submitting.

--- www/main/js/drHome.js
+++ www/main/js/drHome.js
@@ -1,3 +1,7 @@
+var patientToUpdate = "";
+var doctorUsername = "";
+var doctorFullname = "";
+
 /**
  * Called on start up of the webpage. It kicks off the whole thing by retrieving the cookie
  * that was set on sign in or register in order to know who we're dealing with.
@@ -10,9 +14,10 @@ function startUp () {
     console.log(cookie);
     var params = cookie.split("&");
     var uname = params[0].substring(params[0].indexOf('=')+1);
+    doctorUsername = uname;
     var fullname = params[1].substring(params[1].indexOf('=')+1);
-    console.log("uname is: " + uname);
-    console.log("fullname is: " + fullname)
+    doctorFullname = fullname;
+
     $('#name').text(fullname);
 
     makePatientTable (uname);
@@ -48,7 +53,7 @@ function makePatientTable (uname) {
                         "            <td><button type=\"button\" class=\"btn btn-default btn-sm\" onclick='updatePatient(" + "\"" + patients[i]['username'] + "\"" + ")' id=\"updateBut\">\n" +
                         "                <span class=\"glyphicon glyphicon-plus\"></span> Update Patient\n" +
                         "            </button></td>\n" +
-                        "            <td><button type=\"button\" class=\"btn btn-default btn-sm\" onclick='removePatient(" + "\"" + patients[i]['username'] + "\"" + ")' id=\"removeBut\">\n" +
+                        "            <td><button type=\"button\" class=\"btn btn-default btn-sm\" onclick='removePatient(" + "\"" + uname + "\"," + "\"" + patients[i]['username'] + "\"" + ")' id=\"removeBut\">\n" +
                         "                <span class=\"glyphicon glyphicon-minus\"></span> Remove Patient\n" +
                         "            </button></td>"
                     html += "</tr>";
@@ -64,14 +69,78 @@ function makePatientTable (uname) {
 }
 
 //Remove the patient
-function removePatient(patientName) {
+function removePatient(doctorUsername, patientName) {
     console.log("Remove Patient: " + patientName);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "../php/removePatient.php?patientName="+patientName+"&doctorName="+doctorUsername, true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            var res = this.response;
+            var result = JSON.parse(res);
+            console.log(result);
+            if (result['code'] == 100) {
+                location.reload(true);
+            } else {
+                alert("Failed to remove the Patient from your list");
+            }
+        }
+    }
+    xhttp.send();
 }
 
 //Update the patient information
 function updatePatient(patientName) {
     console.log("Update Patient: " + patientName);
+
+    //document.cookie = "patientUsername=" + patientName;
+    patientToUpdate = patientName;
+    window.location.href = "../html/updatePatient.html";
 }
+
+
+function updatePatientInfo(height, weight, age) {
+    console.log("Updating " + patientToUpdate);
+
+    if (height.length > 0 && weight.length > 0 && age.length > 0) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("GET", "../php/updatePatient.php?patientName=" + patientToUpdate + "&height=" + height + "&weight=" + weight + "&age=" + age, true);
+        xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                var res = this.response;
+                var result = JSON.parse(res);
+                console.log(result);
+                if (result['code'] == 100) {
+                    location.reload(true);
+                } else {
+                    alert("Failed to update the Patient information");
+                }
+            }
+        }
+        xhttp.send();
+    } else {
+        if (height.length == 0) {
+            alert("Must specify a height");
+        }
+        if (weight.length == 0) {
+            alert("Must specify a weight");
+        }
+        if (age.length == 0) {
+            alert("Must specify an age");
+        }
+    }
+}
+
+
+function cancel () {
+    document.cookie = document.cookie="username="+doctorUsername+"&fullname="+doctorFullname;
+    window.location.href = "../html/DrHome.html";
+}
+
 
 //Add prescription to patient
 function addPrescription(patientName, doctorName) {
--- www/main/php/updatePatient.php
+++ www/main/php/updatePatient.php
@@ -14,8 +14,34 @@ $username = 'root';
 $password = '';
 
 $uname = $_POST['username'];
-$fullname = $_POST['fullname'];
-$pwd = $_POST['password'];
-$type = $_POST['type'];
+$height = $_POST['height'];
+$weight = $_POST['weight'];
+$age = $_POST['age'];
+
+try {
+
+    $db = new PDO($dsn, $username, $password);
+
+    $select_sql = "DELETE FROM assignto WHERE patientUsername = :patientName AND doctorUsername = :dname";
+    $sql = $db->prepare($select_sql);
+
+    $query = $sql->execute(array(":patientName"=>$patientName, ":dname"=>$dname));
+
+    if ($query) {
+        $result = array("code"=>100, "message"=>"Success");
+    } else {
+        $result = array("code"=>200, "message"=>"Failed");
+    }
+
+    echo json_encode($result);
+
+    $sql = null;
+    $db = null;
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    $result = array("code"=>300, "message"=>"There was an error connecting to the database: $error_message");
+    echo json_encode($result);
+    exit();
+}
 
 ?>
